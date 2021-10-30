fbeb13b7f6d8d0c54169f927e22ffe25d85b5881
DESKTOP-NEAEN93\YHS <poches871026@gmail.com>
2021-10-30 21:56:19 +0900

    ag

--- src/git_project/git2.java
+++ src/git_project/git2.java
@@ -6,5 +6,12 @@ public class git2 {
 	public static void main(String[] args) {
 		
 		System.out.println("dddd");
+		System.out.println("2222222222222");
+		System.out.println("2222222222222");
+		System.out.println("2222222222222");
+		System.out.println("2222222222222");
+		System.out.println("2222222222222");
+		System.out.println("2222222222222");
+		
 	}
 }
