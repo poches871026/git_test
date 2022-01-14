f7816109ddd1e51c0e2fd5b8f55d239b6638d1a8
aaaa <poches871026@gmail.com>
2022-01-14 09:28:39 +0900

    Revert "test"
    This reverts commit 69dd3d0feb1398d5d18d0beac8307b6934988aa0.
    
    # Conflicts:
    #	src/git_project/git.java

--- src/git_project/git.java
+++ src/git_project/git.java
@@ -10,10 +10,6 @@ public class git {
 		System.out.println("git hello222");
 		
 		System.out.println("git hello1111");
-		System.out.println("git hello1111");
-		System.out.println("git hello1111");
-		System.out.println("git hello2222");
-		System.out.println("git hello2222");
 	}
 
 }
